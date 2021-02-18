fa3a3e9736eed2acd8e89d01be66effa70772f87
Levente15 <79134762+Levente15@users.noreply.github.com>
2021-02-18 09:35:10 +0200

    Monogramm

---
+++ Labor_1/2_feladat/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String nevem="Csata Levente";
+	    String mono=" Cs. L.";
+	    nevem+=mono;
+	    System.out.printf(nevem);
+    }
+}
