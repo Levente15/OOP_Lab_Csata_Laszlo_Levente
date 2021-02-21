118511b7142e573e8f8ed6b796167087c082c50b
Levente15 <79134762+Levente15@users.noreply.github.com>
2021-02-21 11:41:37 +0200

    4_feladat

--- Labor_1/3_feladat/src/com/company/Main.java
+++ Labor_1/3_feladat/src/com/company/Main.java
@@ -2,16 +2,6 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-        // I Egyszeru main - 1Feladat
-        System.out.println("Jozsi");
-
-        // 2Feladat
-        String nev = "Antal Jozsef";
-        for (int i = 0; i < nev.length(); ++i) {
-            System.out.println(nev.charAt(i));
-        }
-
-        System.out.printf("\n");
 
         // 3Feladat
         String szo = "ALMAFA";
@@ -26,24 +16,7 @@ public class Main {
             System.out.printf("%c\n", szo.charAt(i));
         }
 
-        System.out.printf("\n");
 
-        // 4feladat
-
-        String[] tomb;
-        String nev2 = "Kerekes Bálint Ádám József";
-        tomb = nev2.split(" ");
-
-        for (int i=0; i < tomb.length; i++) {
-            System.out.printf("%c",tomb[i].charAt(0));
-        }
-
-        System.out.printf("\n\n");
-
-        // II Fugvenyenk - 1feladat
-
-        double x[] ={7, 1, -3, 45, 9};
-        System.out.printf("MAX: %6.2f\n", maxElement( x ));
 
     }
 }
