4909b60387e0975defc5a4ab4802909c61d1dfb7
Levente15 <79134762+Levente15@users.noreply.github.com>
2021-04-29 09:36:58 +0300

    Labor_9

---
+++ Labor_9/Labor_9_2/src/Company.java
@@ -0,0 +1,9 @@
+
+public class Company {
+    private int ID;
+    private String firtsName;
+    private String lastName;
+    private double salary;
+    private MyDate birthday;
+    private int counter;
+}
