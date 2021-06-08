bf6df07a38e904ef143fbcda1911cc8d2fbb5ead
Levente15 <79134762+Levente15@users.noreply.github.com>
2021-06-08 18:03:34 +0300

    2

---
+++ Osszes/osszeadas/src/irodalom/Main.java
@@ -0,0 +1,17 @@
+package irodalom;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 123;
+        System.out.println("Az osszeg: " + osszead(a, b));
+    }
+
+    public static int osszead(int a, int b) {
+        int sum;
+        sum = a + b+a;
+
+        return sum;
+    }
+}
